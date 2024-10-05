bfd2c9b94da7797ff1efa6117b077282f421147e
Dakori9480 <kgh9480@naver.com>
2024-10-05 19:01:21 +0900

    Create chat_memory_3.py

---
+++ 04_memory/chat_memory_3.py
@@ -0,0 +1,35 @@
+import os
+import chainlit as cl
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.memory import RedisChatMessageHistory
+from langchain.chains import ConversationChain
+
+chat = ChatOpenAI(
+    model = "gpt-3.5-turbo"
+)
+
+history = RedisChatMessageHistory(
+    session_id="chat_history",
+    url=os.environ.get("REDIS_URL"),
+)
+
+
+memory = ConversationBufferMemory(
+    return_messages=True,
+    chat_memory=history,
+)
+
+chain = ConversationChain(
+    memory= memory,
+    llm=chat,
+)
+
+@cl.on_chat_start
+async def on_chat_start():
+    await cl.Message(content="메세지를 입력하세요.").send()
+
+@cl.on_message
+async def on_message(message : str):
+    result = chain(message)
+    await cl.Message(content=result["response"]).send()
