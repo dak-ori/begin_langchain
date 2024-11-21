0a5a1763792da35cc56fa5c979ac8aaf6ec88bb5
Dakori9480 <kgh9480@naver.com>
2024-11-21 16:39:33 +0900

    Update project02.py

--- 00_example/project02.py
+++ 00_example/project02.py
@@ -1,6 +1,7 @@
 from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage
+import pyttsx3
 import warnings
 
 warnings.filterwarnings("ignore")
@@ -8,6 +9,8 @@ warnings.filterwarnings("ignore")
 chat = ChatOpenAI(
     model = "gpt-4o",
 )
+
+engine = pyttsx3.init()
 
 input_text = input("어떤 물건을 분리수거 하시나요? : ")
 
@@ -24,4 +27,11 @@ result = chat( # 실행
         HumanMessage(content=prompt.format(product=input_text),)
     ]
 )
+
+text = result.content
+engine.setProperty('rate', 150)
+engine.setProperty('volume', 1)
+
 print(result.content)
+engine.say(text)
+engine.runAndWait()
